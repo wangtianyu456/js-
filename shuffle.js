d8951e5998e4f7971354b63a554a4c85362d0cc4
wangtianyu456 <34847604+wangtianyu456@users.noreply.github.com>
2021-08-03 15:06:32 +0800

    add: shuffle

---
+++ shuffle.js
@@ -0,0 +1,9 @@
+function shuffle(list) {
+  const len = list.length;
+  let result = [...list];
+  for (let i = len - 1; i > 0; i--) {
+    const swapIndex = Math.floor(Math.random() * (i + 1));
+    [result[i], result[swapIndex]] = [result[swapIndex], result[i]];
+  }
+  return result;
+}
