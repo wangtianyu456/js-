f63a1fa9ac6076350aae95c0d017b4ccd739d9df
wangtianyu456 <34847604+wangtianyu456@users.noreply.github.com>
2021-08-03 16:15:17 +0800

    add: 随机验证码方法 sample

---
+++ sample.js
@@ -0,0 +1,4 @@
+// 从数组中随机取一个元素
+function sample(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
--- shuffle.js
+++ shuffle.js
@@ -7,3 +7,15 @@ function shuffle(list) {
   }
   return result;
 }
+
+const random = (n) => Math.floor(Math.random() * (n + 1));
+
+// 可以重复的 6位 验证码
+function randomCode() {
+  return [0, 0, 0, 0, 0, 0].map(() => random(9));
+}
+
+// 不可以重复的 6 位 验证码
+function uniqueRandomCode() {
+  return shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).slice(6);
+}
